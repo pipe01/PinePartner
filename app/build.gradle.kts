d6e6ac9c8c2bbb0e10f7bd2cd93dac5dedefd7b0
Felipe Martinez <pipernene@gmail.com>
2024-03-23 13:13:51 +0100

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "net.pipe01.pinepartner"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "0.0.1"
+        versionCode = 2
+        versionName = "0.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
